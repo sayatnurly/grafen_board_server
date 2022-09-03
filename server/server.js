58e48a973d8987b2348a6cbe3ffa741339b5ea52
GDK1995 <gulminez_95@mail.ru>
2022-09-03 16:18:45 +0600

    socket server

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const socketIO = require('socket.io')
+const http = require('http').createServer(app)
+const io = socketIO(http, {
+    cors: {
+      origin: "*",
+    },
+  })
+
+const port = process.env.PORT || 3000
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} has connectd`)
+
+    socket.on('draw', (data) => {
+        socket.broadcast.emit('data', { x: data.x, y: data.y, strokeWidth: data.strokeWidth, strokeColor: data.strokeColor })
+    })
+
+    socket.on('up', (data) => {
+        socket.broadcast.emit('onUp', data)
+    })
+
+    socket.on('down', (data) => {
+        socket.broadcast.emit('onDown', { x: data.x, y: data.y })
+    })
+    
+    socket.on('reset', (data) => {
+        socket.broadcast.emit('onReset', { cnvWidth: data.cnvWidth, cnvHeight: data.cnvHeight })
+    })
+
+    socket.on('disconnect', (socket) => console.log(`${socket.id} has disconnected`))
+})
+
+http.listen(port, () => {
+  console.log(`server has been started on port ${port}`)
+})
