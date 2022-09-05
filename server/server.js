0f122e889effae15e1fcbb58dd30126a04397e54
GDK1995 <gulminez_95@mail.ru>
2022-09-05 10:13:56 +0600

    serv

--- server/server.js
+++ server/server.js
@@ -12,21 +12,26 @@ const port = process.env.PORT || 3000
 
 io.on('connection', (socket) => {
     console.log(`${socket.id} has connectd`)
+    
+    socket.on('createRoom', (room) => {
+        console.log(room)
+        socket.join(room)
+    })
 
     socket.on('draw', (data) => {
-        socket.broadcast.emit('data', { x: data.x, y: data.y, strokeWidth: data.strokeWidth, strokeColor: data.strokeColor })
+        socket.in(room).broadcast.emit('data', { x: data.x, y: data.y, strokeWidth: data.strokeWidth, strokeColor: data.strokeColor })
     })
 
     socket.on('up', (data) => {
-        socket.broadcast.emit('onUp', data)
+        socket.in(room).broadcast.emit('onUp', data)
     })
 
     socket.on('down', (data) => {
-        socket.broadcast.emit('onDown', { x: data.x, y: data.y })
+        socket.in(room).broadcast.emit('onDown', { x: data.x, y: data.y })
     })
     
     socket.on('reset', (data) => {
-        socket.broadcast.emit('onReset', { cnvWidth: data.cnvWidth, cnvHeight: data.cnvHeight })
+        socket.in(room).broadcast.emit('onReset', { cnvWidth: data.cnvWidth, cnvHeight: data.cnvHeight })
     })
 
     socket.on('disconnect', (socket) => console.log(`${socket.id} has disconnected`))
