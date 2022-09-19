a4f316ac04d98fc59971b4ab8082fe5c5580c4a0
GDK1995 <gulminez_95@mail.ru>
2022-09-19 18:43:52 +0600

    text edit

--- server/server.js
+++ server/server.js
@@ -11,26 +11,34 @@ const io = socketIO(http, {
 const port = 3000
 
 io.on('connection', (socket) => {
-    console.log(`${socket.id} has connectd`)
+    // console.log(`${socket.id} has connectd`)
 
     socket.on('openRoom', (room) => {
       console.log('room', socket.id, room)
       socket.join(room)
 
       socket.on('draw', (data) => {
-          socket.broadcast.to(room).emit('data', { x: data.x, y: data.y, strokeWidth: data.strokeWidth, strokeColor: data.strokeColor })
+        socket.broadcast.to(room).emit('data', { x: data.x, y: data.y, strokeWidth: data.strokeWidth, strokeColor: data.strokeColor })
       })
 
       socket.on('up', (data) => {
-          socket.broadcast.to(room).emit('onUp', data)
+        socket.broadcast.to(room).emit('onUp', data)
       })
 
       socket.on('down', (data) => {
-          socket.broadcast.to(room).emit('onDown', { x: data.x, y: data.y })
+        socket.broadcast.to(room).emit('onDown', { x: data[0], y: data[1], width: data[2], height: data[3] })
+      })
+
+      socket.on('text', (data) => {
+        socket.broadcast.to(room).emit('onText', { letter: data[0], x: data[1], y: data[2] })
+      })
+
+      socket.on('undoText', (data) => {
+        socket.broadcast.to(room).emit('onUndoText', data)
       })
 
       socket.on('reset', (data) => {
-          socket.broadcast.to(room).emit('onReset', { cnvWidth: data.cnvWidth, cnvHeight: data.cnvHeight })
+        socket.broadcast.to(room).emit('onReset', { cnvWidth: data.cnvWidth, cnvHeight: data.cnvHeight })
       })
     })
 
